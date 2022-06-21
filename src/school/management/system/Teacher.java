0408cf6aec75ff5dfd5ef4cad417634e87082957
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-21 17:13:27 -0500

    Create Teacher class
    with POJOs

---
+++ src/school/management/system/Teacher.java
@@ -0,0 +1,32 @@
+package school.management.system;
+
+/**
+ * This class maintains data related to the teacher class
+ * -id
+ * -name
+ * -salary
+ */
+public class Teacher {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Teacher(int id, String name, double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+}
