61f5d4a23a95cf333c011399e34fc650c304137e
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-21 17:39:32 -0500

    Update School class
    adding POJO

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -1,10 +1,49 @@
 package school.management.system;
 
+import java.util.List;
+
 public class School {
-    private Teacher teacher;
-    private Student student;
+    private List<Teacher> teachers;
+    private List<Student> students;
     private double totalMoneyEarned;
     private double totalMoneySpent;
 
-    
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public double getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public void setTotalMoneyEarned(double totalMoneyEarned) {
+        this.totalMoneyEarned = totalMoneyEarned;
+    }
+
+    public double getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void setTotalMoneySpent(double totalMoneySpent) {
+        this.totalMoneySpent = totalMoneySpent;
+    }
 }
