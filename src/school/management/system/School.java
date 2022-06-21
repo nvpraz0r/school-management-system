be67a54d7badfc40d47e5d51fca1d70a4c18cc60
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-21 17:21:24 -0500

    Create School class
    declare private variables; private teacher & student objects

---
+++ src/school/management/system/School.java
@@ -0,0 +1,10 @@
+package school.management.system;
+
+public class School {
+    private Teacher teacher;
+    private Student student;
+    private double totalMoneyEarned;
+    private double totalMoneySpent;
+
+    
+}
