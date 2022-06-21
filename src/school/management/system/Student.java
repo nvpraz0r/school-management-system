e0598d726cdff93cbc67f87d89f5a3a7a06c17e3
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-21 15:37:34 -0500

    Created Student class
    created class and constructor with pre-initialized values for student fees

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,38 @@
+package school.management.system;
+
+/**
+ *
+ * This class maintains data related to students
+ *  -id
+ *  -name
+ *  -grade (GPA too)
+ *  -fees (paid)
+ *
+ */
+
+public class Student {
+
+    private int id;
+    private String name;
+    private double grade;
+    private double feesPaid;
+    private double feesTotal;
+
+    /**
+     *
+     * @param id unique identifier for the student
+     * @param name name of the student
+     * @param grade grade received by the student
+     */
+    public Student(int id, String name, double grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        // initial payment values
+        feesPaid = 0.00;
+        feesTotal = 3000.00;
+    }
+
+
+
+}
