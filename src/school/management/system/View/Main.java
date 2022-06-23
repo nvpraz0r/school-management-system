f1fa2e5b138a9050fe78b4674ceb2426150542a3
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-22 20:12:14 -0500

    Update Main
    implementing a menu system to modify data

--- src/school/management/system/View/Main.java
+++ src/school/management/system/View/Main.java
@@ -2,24 +2,54 @@ package school.management.system.View;
 
 import school.management.system.Model.GenerateData;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         // generate the data to be interacted with
         GenerateData generate = new GenerateData();
         generate.generateData();
 
-        /**
-         * display menu
-         *  -view student/teacher
-         *  -modify something
-         *
-         */
+        String userInput = "";
+
+        do {
+            displayMenu();
+            userInput = sc.next();
+            sc.nextLine();
+
+            switch (userInput.toLowerCase()){
+                case "s":
+                    studentSubMenu();
+                case "t":
+                    teacherSubMenu();
+                default:
+                    System.out.println("Unknown command. Please try again.");
+            }
 
 
+        } while (!userInput.toLowerCase().equals("e"));
     }
 
-    public void displayMenu(){
+    public static void displayMenu(){
+        System.out.println("\n");
+        System.out.println("===============");
+        System.out.println("School Management System");
+        System.out.println("Main Menu");
+        System.out.println("S. - Find Student");
+        System.out.println("T. - Find Teacher");
+        System.out.println("E. - Exit");
+        System.out.println("===============");
+        System.out.print("Command: ");
+    }
 
+    public static void studentSubMenu(){
+        System.out.println("Student Sub Menu");
+    }
+
+    public static void teacherSubMenu(){
+        System.out.println("Teacher Sub Menu");
     }
 }
